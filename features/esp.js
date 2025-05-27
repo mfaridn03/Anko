c90216ab902fced1ddc4580a36a384158998f9c6
Farid <mfaridn03@gmail.com>
2025-05-27 21:17:12 +0800

    feat: removed prediction

--- features/esp.js
+++ features/esp.js
@@ -6,8 +6,6 @@ import RenderLib from "../../RenderLibV2J"
 const EntityOtherPlayerMP = Java.type("net.minecraft.client.entity.EntityOtherPlayerMP")
 let currentBoss = null
 let invisStart = null
-let predictedLand = null
-let invisTicks = 0
 
 register("attackEntity", (entity, _) => {
     if (entity.getEntity() instanceof EntityOtherPlayerMP && entity.getName().includes("Bloodfiend")) {
@@ -21,24 +19,6 @@ registerWhen(
             if (currentBoss.isInvisible() && !invisStart) {
                 invisStart = { x: currentBoss.getX(), y: currentBoss.getY(), z: currentBoss.getZ() }
             }
-            else if (invisStart && currentBoss.isInvisible()) {
-                invisTicks++
-                const currentPos = { x: currentBoss.getX(), y: currentBoss.getY(), z: currentBoss.getZ() }
-                if (invisTicks === 6) {
-                    // after 6 ticks boss will travel ~3.3x the 2d distance he did in the first 6 ticks
-                    predictedLand = {
-                        x: invisStart.x + (currentPos.x - invisStart.x) * 3.3,
-                        y: invisStart.y,
-                        z: invisStart.z + (currentPos.z - invisStart.z) * 3.3
-                    }
-                }
-            }
-            else if (invisStart && !currentBoss.isInvisible()) {
-                invisStart = null
-                invisTicks = 0
-                predictedLand = null
-            }
-            return
         }
         else if (currentBoss && currentBoss.isDead()) {
             currentBoss = null
@@ -50,11 +30,11 @@ registerWhen(
 registerWhen(
     register("renderWorld", () => {
         if (currentBoss) {
-            if (invisStart) {
+            if (invisStart && settings.spectrePath) {
                 RenderLib.drawLine(
                     invisStart.x, invisStart.y, invisStart.z,
                     currentBoss.getRenderX(), invisStart.y, currentBoss.getRenderZ(),
-                    1, 1, 1, 0.7, true, 10
+                    1, 1, 1, 0.8, true, 10
                 )
             }
 
@@ -64,15 +44,6 @@ registerWhen(
                 0, 1, 1, 1, true, 2
             )
         }
-
-        if (predictedLand) {
-            RenderLib.drawInnerEspBoxV2(
-                predictedLand.x - 1, invisStart.y - 1, predictedLand.z - 1,
-                2, 2, 2,
-                1, 0, 1,
-                0.3, false
-            )
-        }
     }),
-    () => LocationUtils.getLocation() === "Stillgore Chteau" && (settings.mobESP || settings.vampireESP)
+    () => LocationUtils.getLocation() === "Stillgore Chteau" && (settings.mobESP || settings.vampireESP || settings.spectrePath)
 )
