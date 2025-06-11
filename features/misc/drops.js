4e5f2fb8a2ce7beac57b584124640143e6f0602c
Farid <mfaridn03@gmail.com>
2025-06-12 01:53:54 +0800

    fix: various drop message formatting bugs

--- features/misc/drops.js
+++ features/misc/drops.js
@@ -4,17 +4,16 @@ import LocationUtils from "../../utils/location"
 const ToolKit = Java.type("java.awt.Toolkit")
 const StringSelection = Java.type("java.awt.datatransfer.StringSelection")
 
-register("chat", (event) => {
+register("chat", (rarity, drop, mf, event) => {
     if (LocationUtils.getLocation() !== "Stillgore Chteau") return
-    if (!settings.bundleDropMessage || !settings.copyRNGDrops) return
+    if (!settings.bundleDropMessage && !settings.copyRNGDrops) return
 
     let message = ChatLib.getChatMessage(event, true)
-    if (!message.includes("§6Enchanted Book Bundle")) return // leg rarity
-
-    cancel(event)
+    if (message.includes("Enchanted Book Bundle") && !message.includes("&6Enchanted Book Bundle")) return // leg rarity
 
     if (settings.bundleDropMessage) {
-        message = message.replace("§6Enchanted Book Bundle", "§6The One IV Bundle")
+        cancel(event)
+        message = message.replace("&6Enchanted Book Bundle", "§6The One IV Bundle")
         ChatLib.chat(message)
     }
 
@@ -22,8 +21,8 @@ register("chat", (event) => {
         const selection = new StringSelection(ChatLib.removeFormatting(message))
         const clipboard = ToolKit.getDefaultToolkit().getSystemClipboard()
         clipboard.setContents(selection, null)
+        ChatLib.chat("§7Copied to clipboard!")
     }
-
 
 }).setCriteria(/^(VERY|CRAZY) RARE DROP\! \((Enchanted Book Bundle|Unfanged Vampire Part)\) \(\+(\d+)% ✯ Magic Find\)$/)
 // ^§r§9§lVERY RARE DROP! §r§7(§r§f§r§6Enchanted Book Bundle§r§7) §r§b(+41% §r§b✯ Magic Find§r§b)§r
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import settings from "./config/settings"
 import "./test/test"
 import "./features/visual/esp"
 // import "./features/ichor"
+import "./features/misc/drops"
 
 register("command", () => {
     settings.openGUI()
