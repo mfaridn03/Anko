6fd13ffad4e68c5417efbf9a7472387a7c2164df
Farid <mfaridn03@gmail.com>
2025-06-14 21:04:01 +0800

    feat: command alias (/ak)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import settings from "./config/settings"
 import "./test/test"
 import "./features/visual/esp"
 // import "./features/ichor"
 import "./features/misc/drops"
 import "./features/party/announce"
+import "./features/party/partycommands"
 
 import vampire from "./utils/vampire"
 
 register("command", () => {
     vampire.settings()
-}).setName("anko")
+}).setName("anko").setAliases(["ak"])
